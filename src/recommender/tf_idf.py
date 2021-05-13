8138e149bc1ca9614eee53c9d99acf6cb717cee8
magnus <magnurr@stud.ntnu.no>
2021-05-13 13:59:50 +0200

    WIP: Add updated data and cosine calculation

--- src/recommender/tf_idf.py
+++ src/recommender/tf_idf.py
@@ -1,6 +1,7 @@
-# import nltk
+import nltk
 # from sklearn.metrics import confusion_matrix
-# from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 # from sklearn import svm
 # import csv
 # import pydantic.json
@@ -10,38 +11,58 @@
 # from tabulate import tabulate
 from stemmer import get_stemmed_descriptions
 import json
+from itertools import chain
 
 
 def load_data():
-    with open("src/cleaning/stemmed_data.json") as file:
+    with open("../cleaning/stemmed_data.json") as file:
         data = json.load(file)
     return data
 
 
-def split_descriptions_on_language(objects):
+def split_theses_on_language(objects):
+    english_theses = {}
+    norwegian_theses = {}
     for key, obj in objects.items():
-        print(obj[1])
+        if obj[1] == "no":
+            norwegian_theses[key] = obj[0]
+        else:
+            english_theses[key] = obj[0]
+    return english_theses, norwegian_theses
 
 
 def main():
     # stemmer
     objects = load_data()
-    split_descriptions_on_language(objects)
-    # # We split the dataset 50/50, as there are relatively few tweets from
-    # # Musk (about 2800) out of the full number of tweets (almost 60000)
+    english_theses, norwegian_theses = split_theses_on_language(objects)
+
+    # We split the dataset 50/50
     # X_train, X_test, y_train, y_test = train_test_split(
     #     cleaned_texts, labels, test_size=0.5, random_state=420)
 
     # # Then we fit and vectorize the training set with TF-IDF representation
-    # vectorizer = TfidfVectorizer()
-    # X_train = vectorizer.fit_transform(X_train)
+    vectorizer = TfidfVectorizer()
+    english_terms = [" ".join(thesis) for thesis in english_theses.values()]
+    english_tfs = vectorizer.fit_transform(english_terms)
+    # print(english_tfs[0:1])
+    test_query = vectorizer.transform(["nlp machine learning"])
+    # print("\ntest_query:")
+    # print(test_query)
+    cosine_similarities = linear_kernel(test_query, english_tfs).flatten()
+    print(cosine_similarities)
+    related_docs_indices = cosine_similarities.argsort()[:-5:-1]
+    print(related_docs_indices)
+    print(cosine_similarities[related_docs_indices])
+    print(english_tfs[0])
+    # print(vectorizer.get_feature_names())
 
     # # We then train a SVM on the vectorized data
     # svc = svm.SVC(C=1000, probability=True)
     # svc.fit(X_train, y_train)
 
-    # # Finally, we evaluate the performance
-    # X_test_vectorized = vectorizer.transform(X_test)
+    # Finally, we evaluate the performance
+    #
+
     # y_pred = svc.predict(X_test_vectorized)
 
     # # We then try to predict the probability that
